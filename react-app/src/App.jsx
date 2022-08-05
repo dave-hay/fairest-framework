5515a9d0d7f0196630f2d2d6294a5152c58b0a34
hayitsdavid <davehay93@gmail.com>
2022-08-05 14:49:19 -0700

    add 10k react only works once

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -4,6 +4,10 @@ import Todo from "./Todo";
 function App() {
   const [todos, setTodos] = useState([]);
   const [inp, setInp] = useState("");
+  const [totalTime, setTotalTime] = useState("");
+  const [totalItems, setTotalItems] = useState("");
+  const [loading, setLoading] = useState(false);
+
   const handleClick = (e) => {
     e.preventDefault();
     const newTodo = { name: inp };
@@ -11,20 +15,47 @@ function App() {
     setInp("");
   };
 
+  const addXItems = (num) => {
+    setLoading(true);
+    const t0 = performance.now();
+    // for (let i = 0; i < num; i++) {
+    //   newItems.push({ name: inp });
+    //   // setTotalItems(totalItems + 1);
+    // }
+    const newI = Array(num).fill({ name: inp });
+    setTodos(...todos, newI);
+    const t1 = performance.now();
+    const time = (t1 - t0) / 10000;
+    setLoading(false);
+    setTotalTime(`${time.toFixed(3)} seconds`);
+    setTotalItems(+totalItems + num);
+  };
+
   return (
     <div>
       <div>
         <h1>My Todos</h1>
+        <p>Total Time: {totalTime}</p>
+        <p>Total Items: {totalItems}</p>
         <input
           type="text"
           value={inp}
           onChange={(e) => setInp(e.target.value)}
         />
         <button type="button" onClick={handleClick}>Add</button>
+        <button
+          type="button"
+          onClick={(e) => {
+            e.preventDefault();
+            addXItems(1000);
+          }}
+        >
+          Add 10K
+        </button>
       </div>
       <div>
         <ul>
-          {todos.map((todo) => <Todo props={todo} />)}
+          {!loading && todos.map((todo) => <Todo props={todo} />)}
         </ul>
       </div>
     </div>
