fffe8bd9efffc2b0fa8e4a963a5151f235af1063
hayitsdavid <davehay93@gmail.com>
2022-08-08 16:13:01 -0700

    vue: basic todo

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,21 +1,42 @@
 <script>
+
+let id = 0;
+
 export default {
   name: 'App',
   data() {
     return {
-      count: 0
-    }
+      newTodo: '',
+      todos: [
+        { id: id++, text: 'item 1' },
+        { id: id++, text: 'item 2' },
+        { id: id++, text: 'item 3' },
+      ],
+    };
   },
   methods: {
-    increment() {
-      this.count++
-    }
-  }
-}
+    addTodo() {
+      this.todos.push({ id: id++, text: this.newTodo });
+      this.newTodo = '';
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo);
+    },
+  },
+};
 </script>
 
 <template>
-  <button @click='increment'>count is : {{count}}</button>
+  <form @submit.prevent='addTodo'>
+    <input v-model='newTodo'>
+    <button>Add Todo</button>
+  </form>
+  <ul>
+    <li v-for='todo in todos' :key='todo.id'>
+      {{ todo.text }}
+      <button @click='removeTodo(todo)'>x</button>
+    </li>
+  </ul>
 </template>
 
 <style>
@@ -23,8 +44,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  list-style: none;
 }
 </style>
