7135265cca5c85933779e57627c21f944c582082
hayitsdavid <davehay93@gmail.com>
2022-08-16 13:49:26 -0700

    svelte: add10k

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -3,27 +3,32 @@
 
   let todoList = [];
 
-  function addToList() {
-    if (!newItem) return
-    todoList = [...todoList, {text: newItem, status: false}];
-    newItem = '';
+  function removeFromList(index) {
+    todoList.splice(index, 1);
+    todoList = todoList;
   }
 
-  function removeFromList(index) {
-    todoList.splice(index, 1)
-    todoList = todoList;
+  function addXToList(num) {
+    if (!newItem) return;
+    let i = 0;
+    while (i < num) {
+      todoList = [...todoList, { text: newItem, status: false }];
+      i++;
+    }
+    newItem = '';
   }
 </script>
 
-<input bind:value={newItem} type="text" placeholder="new todo item..">
-<button on:click={addToList}>Add</button>
+<input bind:value={newItem} type='text' placeholder='new todo item..'>
+<button on:click={() => addXToList(1)}>Add</button>
+<button on:click={() => addXToList(10000)}>Add10K</button>
 
-<br/>
+<br />
 {#each todoList as item, index}
-  <input bind:checked={item.status} type="checkbox">
+  <input bind:checked={item.status} type='checkbox'>
   <span class:checked={item.status}>{item.text}</span>
   <button on:click={() => removeFromList(index)}>X</button>
-  <br/>
+  <br />
 {/each}
 
 
