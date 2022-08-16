2839ba5879216c994fb609e8ab6a1ec67f84a95e
hayitsdavid <davehay93@gmail.com>
2022-08-16 14:25:34 -0700

    svelte: all periods of time

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -16,10 +16,12 @@
     while (i < num) {
       todoList = [...todoList, { text: newItem, status: false }];
       i++;
+      if (i % 10) console.log(i);
     }
     newItem = '';
     const t1 = performance.now();
-    totalTime = (t1 - t0);
+    let time = (t1 - t0);
+    totalTime = time < 1000 ? time.toFixed(3) + " ms" : (time / 1000) + " s"
     totalItems = todoList.length;
   }
 </script>
@@ -31,7 +33,11 @@
 
 <input bind:value={newItem} type='text' placeholder='new todo item..'>
 <button on:click={() => addXToList(1)}>Add</button>
+<button on:click={() => addXToList(1000)}>Add1K</button>
+<button on:click={() => addXToList(5000)}>Add5K</button>
 <button on:click={() => addXToList(10000)}>Add10K</button>
+<button on:click={() => addXToList(100000)}>Add100K</button>
+<button on:click={() => addXToList(500000)}>Add500K</button>
 
 <br />
 {#each todoList as item, index}
