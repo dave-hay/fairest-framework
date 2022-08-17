a3196491d99a82a2be87d5a83027e10ed559c893
hayitsdavid <davehay93@gmail.com>
2022-08-17 16:05:59 -0700

    py: script works for yarn build!

--- build_site.py
+++ build_site.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python3
 
-# import os
+import os
 from pathlib import Path
-import subprocess
+# import subprocess
 
 # os.system('echo "hello world"')
 ###
 # print(os.getcwd())
 # print(os.path.relpath(os.curdir))
+# subprocess.Popen([f'cd {dir}', "yarn build", ".."])
 ###
 
 p = Path('.')
-dirs = [str(x) for x in p.iterdir() if x.is_dir() if 'app' in str(x)]
+dirs = [x for x in p.iterdir() if x.is_dir() if 'app' in str(x)]
 
 for dir in dirs:
-    subprocess.run([f'cd {dir}', "yarn build", ".."])
+    prev = Path.cwd()
+    os.chdir(prev / dir)
+    os.system('yarn build')
+    os.chdir(prev)
 
 # print(idk)
