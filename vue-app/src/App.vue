da66d3e36ac91ab94f79ec7107c7d4f768d7d21b
hayitsdavid <davehay93@gmail.com>
2022-08-17 17:20:00 -0700

    vue: added buttons

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,48 +1,48 @@
 <script>
-
 let id = 0;
 
 export default {
-  name: 'App',
+  name: "App",
   data() {
     return {
-      newTodo: '',
-      todos: [
-        { id: id++, text: 'item 1' },
-        { id: id++, text: 'item 2' },
-        { id: id++, text: 'item 3' },
-      ],
+      newTodo: "",
+      todos: [],
     };
   },
   methods: {
     addTodo() {
       this.todos.push({ id: id++, text: this.newTodo });
-      this.newTodo = '';
+      this.newTodo = "";
     },
     removeTodo(todo) {
       this.todos = this.todos.filter((t) => t !== todo);
     },
-    add10K() {
-      for (let i = 0; i < 10000; i++) {
+    addXItems(num) {
+      for (let i = 0; i < num; i++) {
         this.todos.push({ id: id++, text: this.newTodo });
       }
-      this.newTodo = '';
+      this.newTodo = "";
     },
   },
 };
 </script>
 
 <template>
+  <img alt="Vue logo" src="./assets/logo.png" />
   <h1>My Todos</h1>
-  <form @submit.prevent='addTodo'>
-    <input v-model='newTodo'>
+  <form @submit.prevent="addTodo">
+    <input v-model="newTodo" />
     <button>Add Todo</button>
-    <button @click='add10K'>Add 10K</button>
+    <button @click="addXItems(1000)">Add 1K</button>
+    <button @click="addXItems(5000)">Add 5K</button>
+    <button @click="addXItems(10000)">Add 10K</button>
+    <button @click="addXItems(50000)">Add 50K</button>
+    <button @click="addXItems(100000)">Add 100K</button>
   </form>
   <ul>
-    <li v-for='todo in todos' :key='todo.id'>
+    <li v-for="todo in todos" :key="todo.id">
       {{ todo.text }}
-      <button @click='removeTodo(todo)'>x</button>
+      <button @click="removeTodo(todo)">x</button>
     </li>
   </ul>
 </template>
@@ -55,5 +55,19 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
   list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+a {
+  color: #42b983;
 }
 </style>
