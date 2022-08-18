175cf0762ee362f713a76559aad5680620d9e954
hayitsdavid <davehay93@gmail.com>
2022-08-17 17:53:28 -0700

    vue: total time + items

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,5 +1,7 @@
 <script>
 let id = 0;
+/* let totalTime = 0;
+let totalItems = 0; */
 
 export default {
   name: "App",
@@ -7,21 +9,25 @@ export default {
     return {
       newTodo: "",
       todos: [],
+      totalTime: "0 ms",
+      totalItems: 0,
     };
   },
   methods: {
-    addTodo() {
-      this.todos.push({ id: id++, text: this.newTodo });
-      this.newTodo = "";
-    },
-    removeTodo(todo) {
-      this.todos = this.todos.filter((t) => t !== todo);
-    },
     addXItems(num) {
+      let t0 = performance.now();
       for (let i = 0; i < num; i++) {
         this.todos.push({ id: id++, text: this.newTodo });
       }
       this.newTodo = "";
+      let t1 = performance.now();
+      let time = t1 - t0;
+      this.totalTime =
+        time < 1000 ? time.toFixed(3) + " ms" : time / 1000 + " s";
+      this.totalItems = this.todos.length;
+    },
+    removeTodo(todo) {
+      this.todos = this.todos.filter((t) => t !== todo);
     },
   },
 };
@@ -30,9 +36,11 @@ export default {
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <h1>My Todos</h1>
+  <p>Total Time: {{ totalTime }}</p>
+  <p>Total Items: {{ totalItems }}</p>
   <form @submit.prevent="addTodo">
     <input v-model="newTodo" />
-    <button>Add Todo</button>
+    <button @click="addXItems(1)">Add 1</button>
     <button @click="addXItems(1000)">Add 1K</button>
     <button @click="addXItems(5000)">Add 5K</button>
     <button @click="addXItems(10000)">Add 10K</button>
