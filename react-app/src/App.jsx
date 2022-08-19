4b3da8472e358a1e4727752537571908a8568c0d
hayitsdavid <davehay93@gmail.com>
2022-08-19 13:48:11 -0700

    react: check item

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -6,10 +6,14 @@ function App() {
   const [totalTime, setTotalTime] = useState("");
   const [totalItems, setTotalItems] = useState("");
 
-  const checkItem = (td) => {
-    const newTodos = [...todos];
-    newTodos[td.id] = { id: td.id, text: td.text, status: true };
-    setTodos(newTodo);
+  const checkItem = (cur) => {
+    const newTodos = todos.map((t) => {
+      if (t === cur) {
+        t.status = !t.status;
+      }
+      return t;
+    });
+    setTodos(newTodos);
   };
 
   const removeFromList = (td) => {
@@ -21,13 +25,14 @@ function App() {
     const t0 = performance.now();
     const newItems = [];
     for (let i = 0; i < num; i++) {
-      newItems.push({ id: i, text: newTodo, status: false });
+      newItems.push({ text: newTodo, status: false });
     }
     setTodos([...todos, ...newItems]);
     const t1 = performance.now();
     const time = t1 - t0;
-    const toWrite = `${num}: ${time} ms`;
-    console.log(toWrite);
+    // Getting DATA
+    // const toWrite = `${num}: ${time} ms`;
+    // console.log(toWrite);
     const totalTime =
       time < 1000 ? time.toFixed(3) + " ms" : (time / 1000).toFixed(2) + " s";
     setTotalTime(`${totalTime}`);
@@ -47,13 +52,14 @@ function App() {
         />
         <button onClick={() => addXItems(1)}>Add 1</button>
         <button onClick={() => addXItems(1000)}>Add 1K</button>
+        <button onClick={() => addXItems(5000)}>Add 5K</button>
         <button onClick={() => addXItems(10000)}>Add 10K</button>
         <button onClick={() => addXItems(50000)}>Add 50K</button>
         <button onClick={() => addXItems(100000)}>Add 100K</button>
       </div>
       <div>
         <ul>
-          {todos.map((todo, i) => {
+          {todos.map((todo) => {
             return (
               <li>
                 <input type="checkbox" onClick={() => checkItem(todo)} />
