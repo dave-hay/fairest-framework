fdc999aab707ba448efed2accd0d2df6c2c59d93
hayitsdavid <davehay93@gmail.com>
2022-08-19 11:22:54 -0700

    angular: buttons working

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -7,13 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  totalTime = '0 ms';
+  totalItems = 0;
 
-  allItems: { description: string }[] = [];
+  allItems: { description: string; status: boolean }[] = [];
 
   addXItems(items: number, description: string) {
+    let t0 = performance.now();
     for (let i = 0; i < items; i++) {
-      this.allItems.unshift({ description });
+      this.allItems.unshift({ description, status: false });
     }
+    let t1 = performance.now();
+    let time = t1 - t0;
+    this.totalTime = time < 1000 ? time.toFixed(3) + ' ms' : time / 1000 + ' s';
+    this.totalItems = this.todos.length;
+  }
+
+  completeItem(todo: any) {
+    todo.status = !todo.status;
+  }
+
+  removeTodos(todo: any) {
+    this.allItems = this.allItems.filter((t) => t !== todo);
   }
 
   get todos() {
