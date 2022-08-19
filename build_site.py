4baa59fe4c3593c4e57d3c28f15b98b07aba7bfb
hayitsdavid <davehay93@gmail.com>
2022-08-18 23:02:15 -0700

    config: py script removes line from angular

--- build_site.py
+++ build_site.py
@@ -12,8 +12,32 @@ for dir in dirs:
     os.system('yarn build')
     os.chdir(prev)
 
-# TODO: cd to dist dir
-# TODO: angular: rm '<base href="/">'
+dist_path = Path('./dist/')
+dist_dirs = [
+    str(x) for x in dist_path.iterdir() if x.is_dir() if 'app' in str(x)
+]
+
+
+def updateFile(dir):
+    with open(dir, 'r') as fp:
+        for line in fp:
+            if '<base href="/">' in line: continue
+
+
+# angular: rm '<base href="/">'
+for dir in dist_dirs:
+    if 'angular' in dir:
+        ang = []
+        with open(dir + "/index.html", 'r') as fp:
+            for line in fp:
+                if '<base href="/"' in line: continue
+                else: ang.append(line)
+            fp.close()
+        with open(dir + "/index.html", 'w') as fp:
+            for newLine in ang:
+                fp.write(newLine)
+            fp.close()
+
 # TODO: svelte: rm '/' from href=" src="
 # TODO: react: rm <link rel="apple-touch-icon" href="/logo192.png" /><link rel="manifest" href="/manifest.json" />
 # TODO: react: rm '/' from href=" src="
