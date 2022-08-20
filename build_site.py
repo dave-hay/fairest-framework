283d9daccb44ba8c954f9965b91227d67b345aac
hayitsdavid <davehay93@gmail.com>
2022-08-20 13:18:10 -0700

    script: funcs v1

--- build_site.py
+++ build_site.py
@@ -2,61 +2,83 @@
 
 import os
 from pathlib import Path
+import sys
 
-p = Path('.')
-dirs = [x for x in p.iterdir() if x.is_dir() if 'app' in str(x)]
 
-for dir in dirs:
-    prev = Path.cwd()
-    os.chdir(prev / dir)
-    os.system('yarn build')
-    os.chdir(prev)
+def build(framework):
+    p = Path('.')
+    dirs = [x for x in p.iterdir() if x.is_dir() if framework in str(x)]
 
-dist_path = Path('./dist/')
-dist_dirs = [
-    str(x) for x in dist_path.iterdir() if x.is_dir() if 'app' in str(x)
-]
+    for dir in dirs:
+        prev = Path.cwd()
+        os.chdir(prev / dir)
+        os.system('yarn build')
+        os.chdir(prev)
 
-# angular: rm '<base href="/">'
-for dir in dist_dirs:
-    if 'angular' in dir:
-        ang = []
-        with open(dir + "/index.html", 'r') as fp:
-            for line in fp:
-                if '<base href="/"' in line: continue
-                else: ang.append(line)
-            fp.close()
-        with open(dir + "/index.html", 'w') as fp:
-            for newLine in ang:
-                fp.write(newLine)
-            fp.close()
+
+def config(framework):
+    dist_path = Path('./dist/')
+    dist_dirs = [
+        str(x) for x in dist_path.iterdir() if x.is_dir()
+        if framework in str(x)
+    ]
+
+    # angular: rm '<base href="/">'
+    for dir in dist_dirs:
+        if 'angular' in dir:
+            ang = []
+            with open(dir + "/index.html", 'r') as fp:
+                for line in fp:
+                    if '<base href="/"' in line: continue
+                    else: ang.append(line)
+                fp.close()
+            with open(dir + "/index.html", 'w') as fp:
+                for newLine in ang:
+                    fp.write(newLine)
+                fp.close()
 
 # rm <link rel="apple-touch-icon" href="/logo192.png" /><link rel="manifest" href="/manifest.json" />
-    if 'react' in dir:
-        react_html = []
-        with open(dir + "/index.html", 'r') as fp:
-            for line in fp:
-                if '<link rel="apple-touch-icon' in line: continue
-                if '<link rel="manifest' in line: continue
-                else: react_html.append(line)
-            fp.close()
-        with open(dir + "/index.html", 'w') as fp:
-            for newLine in react_html:
-                fp.write(newLine)
-            fp.close()
+        if 'react' in dir:
+            react_html = []
+            with open(dir + "/index.html", 'r') as fp:
+                for line in fp:
+                    if '<link rel="apple-touch-icon' in line: continue
+                    if '<link rel="manifest' in line: continue
+                    else: react_html.append(line)
+                fp.close()
+            with open(dir + "/index.html", 'w') as fp:
+                for newLine in react_html:
+                    fp.write(newLine)
+                fp.close()
+
 
 # svelte, vue, react
-    if 'svelte' in dir or 'vue' in dir or 'react' in dir:
-        html = []
-        with open(dir + "/index.html", 'r') as fp:
-            for line in fp:
-                if 'href="/' in line or 'src="/' in line:
-                    html.append(line.replace('="/', '="'))
-                else:
-                    html.append(line)
-            fp.close()
-        with open(dir + "/index.html", 'w') as fp:
-            for newLine in html:
-                fp.write(newLine)
-            fp.close()
+        if 'svelte' in dir or 'vue' in dir or 'react' in dir:
+            html = []
+            with open(dir + "/index.html", 'r') as fp:
+                for line in fp:
+                    if 'href="/' in line or 'src="/' in line:
+                        html.append(line.replace('="/', '="'))
+                    else:
+                        html.append(line)
+                fp.close()
+            with open(dir + "/index.html", 'w') as fp:
+                for newLine in html:
+                    fp.write(newLine)
+                fp.close()
 
+if __name__ == "__main__":
+    args = sys.argv[1:]  # rm script name
+
+    if len(args) == 0:
+        print('building all apps')
+        build('app')
+        config('app')
+
+    kywds = {'react', 'svelte', 'vue', 'angular'}
+
+    for arg in args:
+        if arg.lower() in kywds:
+            print(f'building {arg}')
+            build(arg.lower())
+            # config(arg.lower())
